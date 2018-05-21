b1cb2d7166cd4fe79217da056d66822f609a50e2
Patrick <patascis@gmail.com>
2018-05-20 20:09:42 -0700

    Add Function constructor, prototype and inheritance

---
+++ 5-Advanced-JS/script.js
@@ -0,0 +1,25 @@
+// Function Constructor
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person.prototype.calculateAge = function () {
+  console.log(2016 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1994, 'programmer');
+var mark = new Person('Mark', 1956, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
